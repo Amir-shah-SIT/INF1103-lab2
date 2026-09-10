a099020b62bb5e8ee08aef2759d6da153690f417
Amir <2601448@sit.singaporetech.edu.sg>
2026-09-10 19:39:56 +0800

    added check for when hit 500

--- auditor.py
+++ auditor.py
@@ -3,18 +3,21 @@ rejected = 0
 quit = False
 while quit == False :
     print("\nCurrent Stock is:" + str(inventory))
-    userInput = input("Input additional stock value or quit:\n")
+    userInput = input("\nInput additional stock value or quit:\n")
     if userInput.isdigit() == True:
         if int(userInput) < 0:
             rejected += 1
             print("\nUnacceptable input")
         else:
             inventory += int(userInput)
-            if inventory > 500:
-                rejectedStock = inventory -500
+            rejectedStock = inventory - 500
+            if inventory >=  500:
                 inventory = 500
-                print("\nInventory exceeds 500 limit quitting program")
-                print("\nRejected Stock: " + str(rejectedStock))
+                if(rejectedStock > 0):
+                    print("\nInventory exceeds 500 limit quitting program")
+                    print("\nRejected Stock: " + str(rejectedStock))
+                else:
+                    print("\nInventory has hit the limit of 500")
                 quit = True
         
     elif userInput.lower() == "quit":
