996f17aac647ecea1af6f0085ae7c7330a49b675
Amir <2601448@sit.singaporetech.edu.sg>
2026-09-18 16:13:15 +0800

    change calculate_tax from print to return

--- modular_auditor.py
+++ modular_auditor.py
@@ -25,7 +25,7 @@ def process_delivery(current_total, new_value):
 
 def calculate_tax(amount):
     tax = amount *.10
-    print("\n The tax amount is: $"+ str(tax))
+    return tax
 
 def generate_report(total_units, failed_attempts):  
     print("\nTotal Deliveries Processed: " + str(total_units))
@@ -37,16 +37,19 @@ while quit == False :
     accepted_Input = get_valid_input()
     if accepted_Input == 'quit':
         generate_report(inventory,rejected)
-        calculate_tax(inventory)
+        tax = calculate_tax(inventory)
+        print("\n The tax amount is: $"+ str(tax))
         break
     else:
         inventory = process_delivery(inventory,accepted_Input)
         if inventory > 500:
             generate_report(500,rejected)
             print("\nNumber of Rejected Stock: "+ str(inventory-500))
-            calculate_tax(500)
+            tax = calculate_tax(500)
+            print("\n The tax amount is: $"+ str(tax))
             break
-        calculate_tax(inventory)
+        tax = calculate_tax(inventory)
+        print("\n The tax amount is: $"+ str(tax))
         
             
     
